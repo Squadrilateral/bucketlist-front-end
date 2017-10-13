1484ce3cc4342639b3a4cac2402e8a02fcb19f3e
ccasner <charlottecasner@gmail.com>
2017-10-13 10:58:24 -0400

    Deletes Curl Request
    Deletes client keys and curl request for yelp api

--- assets/scripts/yelp/get.sh
+++ assets/scripts/yelp/get.sh
@@ -1,9 +1,1 @@
-curl  \
-  -X POST -H "Cache-Control: no-cache"   \
-  -H "Postman-Token:Postman-Token"   \
-  -H "Content-Type: application/x-www-form-urlencoded"   \
-  -d 'client_id=JBYDBoXLsrQcvB7vRmr2cw&client_secret=PdwyyyyBND5bJXs3MPsRRD9BgqSB5xudDWk2nXW3MrY1SjcAaUkjfDRvqANEhbnf&grant_type=client_credentials'   \
-  "https://api.yelp.com/oauth2/token"
 
-
-# {"access_token": "p8d8IV929sZ6XNoIo3deV4YOcQRTxZYQekXTFhGRyrNsy_-2IFWYd3HoCIFORWid4ULMKoJBhzUpWveB-Dxowz4lHTkezdFZ69IYYYw-7k-fijhYM4YLH7lZLJjfWXYx", "expires_in": 639654685, "token_type": "Bearer"}
