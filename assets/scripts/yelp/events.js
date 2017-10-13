80ffe2d12eec5955ad73b3ca2f10bfa9ff3b9c7b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-10-13 00:54:53 -0400

    Update onYelpSearch function   - update on success function and passed in data

--- assets/scripts/yelp/events.js
+++ assets/scripts/yelp/events.js
@@ -4,11 +4,14 @@ const ui = require('./ui')
 const getFormFields = require('../../../lib/get-form-fields')
 const store = require('../store')
 
-const onYelpSearch = function () {
+const onYelpSearch = function (event) {
+  event.preventDefault()
+
+
   console.log('onYelpSearch ran')
   api.getYelpResults()
-    .then((data) => console.log(data))
-    .then(ui.getYelpResultsSuccess)
+    // .then((data) => console.log('on yelp search is ', data))
+    .then(data => ui.getYelpResultsSuccess(data))
     .catch(ui.getYelpResultsFailure)
 }
 
