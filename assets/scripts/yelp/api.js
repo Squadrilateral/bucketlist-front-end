7cc105d1b8ca913f249df834faff216d9169cf17
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-10-13 10:25:00 -0400

    Update yelp api call   - pass data from form to index get request   - log data

--- assets/scripts/yelp/api.js
+++ assets/scripts/yelp/api.js
@@ -1,13 +1,14 @@
 const config = require('../config')
 const store = require('../store')
 
-const getYelpResults = function () {
+const getYelpResults = function (data) {
   return $.ajax({
     url: config.apiOrigin + '/yelpresults',
     method: 'GET',
     headers: {
       Authorization: 'Token token=' + store.user.token
-    }
+    },
+    data
   })
 }
 
--- assets/scripts/yelp/events.js
+++ assets/scripts/yelp/events.js
@@ -6,13 +6,18 @@ const store = require('../store')
 
 const onYelpSearch = function (event) {
   event.preventDefault()
-
-
-  console.log('onYelpSearch ran')
-  api.getYelpResults()
+  const data = getFormFields(this)
+  console.log('onYeldSearch data is', data)
+  // if (data.term.trim().length) {
+    console.log('onYelpSearch ran')
+    api.getYelpResults(data)
     // .then((data) => console.log('on yelp search is ', data))
-    .then(data => ui.getYelpResultsSuccess(data))
-    .catch(ui.getYelpResultsFailure)
+      .then(data => ui.getYelpResultsSuccess(data))
+      .catch(ui.getYelpResultsFailure)
+  // } else {
+    // console.log('name field required for yelp search')
+    // $('.add-listitem-message').html('Name field is require')
+  // }
 }
 
 const addHandlers = function () {
