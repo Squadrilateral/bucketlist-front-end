fe5b0ed1420304857a70bd278d9b9a2960033804
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-10-13 12:45:19 -0400

    Adds handlebars function
    Fills div search content with html
    
    contributors:
    Driver: Charlie
    Navigators: Danny, Charlotte, Phuongnhat

--- assets/scripts/yelp/ui.js
+++ assets/scripts/yelp/ui.js
@@ -1,10 +1,17 @@
 const store = require('../store')
-const showList = require('../templates/listitems.handlebars')
+const showResult = require('../templates/yelpresults.handlebars')
 
 const getYelpResultsSuccess = function (data) {
-  console.log('getting to ui call')
-  console.log('data from api is ', data)
+  store.yelpresults = data.yelpresults
+  // console.log("data.yelpresults", data.yelpresults)
+  const showResultHTML = showResult({ yelpresults: store.yelpresults })
+  $('#search-content').html(showResultHTML)
+
+  // console.log('getting to ui call')
+  // console.log('data from api is ', data)
+  $('#search-content').show()
 }
+
 const getYelpResultsFailure = function (error) {
   console.error(error)
 }
