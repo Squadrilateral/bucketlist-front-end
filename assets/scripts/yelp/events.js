feef6c6d490fe5e428a448ca2b7e8ebf81c15ca2
dannyclark77 <dannyclark77@gmail.com>
2017-10-13 14:31:44 -0400

    Add auto-update feature to add yelp items to bl
    Add auto-populate function to add bucket list items when adding
    a yelp search list item
    
    contributors:
    Driver: Danny
    Navigators: Charlie, Charlotte, Phuongnhat

--- assets/scripts/yelp/events.js
+++ assets/scripts/yelp/events.js
@@ -9,19 +9,41 @@ const onYelpSearch = function (event) {
   const data = getFormFields(this)
   console.log('onYeldSearch data is', data)
   // if (data.term.trim().length) {
-    console.log('onYelpSearch ran')
-    api.getYelpResults(data)
+  console.log('onYelpSearch ran')
+  api.getYelpResults(data)
     // .then((data) => console.log('on yelp search is ', data))
-      .then(data => ui.getYelpResultsSuccess(data))
-      .catch(ui.getYelpResultsFailure)
+    .then(data => ui.getYelpResultsSuccess(data))
+    .catch(ui.getYelpResultsFailure)
   // } else {
     // console.log('name field required for yelp search')
     // $('.add-listitem-message').html('Name field is require')
   // }
 }
 
+const getItemName = function (button) {
+  console.log('Button is ', button.target)
+  const itemName = $(button.target).parent().siblings('.resultName').attr('data-item-name')
+  console.log('itemName is', itemName)
+  return itemName
+}
+
+const getItemLocation = function (button) {
+  console.log('Button is ', button.target)
+  const itemLocation = $(button.target).parent().siblings('.resultLocation').attr('data-item-location')
+  console.log('itemLocation is', itemLocation)
+  return itemLocation
+}
+
+const getCurrentData = function (addButton) {
+  const itemName = getItemName(addButton)
+  const itemLocation = getItemLocation(addButton)
+  $('#listItemName').val(itemName)
+  $('#listItemLocation').val(itemLocation)
+}
+
 const addHandlers = function () {
   $('#yelp-search').on('submit', onYelpSearch)
+  $('#search-content').on('click', '.add-result-button', getCurrentData)
 }
 
 module.exports = {
